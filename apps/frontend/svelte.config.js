603a4eaa2d8a5a5ddff7db583360afcfcd7acb25
MP281X <paludgnachmatteo.dev@gmail.com>
2023-08-18 16:57:52 +0200

    disabled csrf

--- apps/frontend/svelte.config.js
+++ apps/frontend/svelte.config.js
@@ -4,7 +4,10 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		csrf: { checkOrigin: false }
+	}
 };
 
 export default config;
