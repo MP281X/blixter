c6b0176ded234bbd348ddb4a1cd1b216fc9f456a
MP281X <paludgnachmatteo.dev@gmail.com>
2023-08-21 21:02:07 +0200

    prettier

--- apps/frontend/src/routes/(home)/+page.server.ts
+++ apps/frontend/src/routes/(home)/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-  return { user: locals.user.username };
+	return { user: locals.user.username };
 };
--- apps/frontend/src/routes/auth/+page.server.ts
+++ apps/frontend/src/routes/auth/+page.server.ts
@@ -9,63 +9,63 @@ const loginSchema = zodDefault(usersSchema.select.shape);
 const signupSchema = zodDefault(usersSchema.add.shape);
 
 export const load: PageServerLoad = async () => {
-  return { loginSchema, signupSchema };
+	return { loginSchema, signupSchema };
 };
 
 export const actions: Actions = {
-  login: async ({ request, cookies }) => {
-    const formData = usersSchema.select.safeParse(Object.fromEntries(await request.formData()));
+	login: async ({ request, cookies }) => {
+		const formData = usersSchema.select.safeParse(Object.fromEntries(await request.formData()));
 
-    if (!formData.success) return formatZodError(formData.error);
-    const data = formData.data;
+		if (!formData.success) return formatZodError(formData.error);
+		const data = formData.data;
 
-    const res = await db.query.users.findFirst({
-      where: (users, { eq }) => eq(users.username, data.username)
-    });
+		const res = await db.query.users.findFirst({
+			where: (users, { eq }) => eq(users.username, data.username)
+		});
 
-    if (!res || !(await bcrypt.compare(data.password, res.password))) return fail(400, { error: 'invalid username or password' });
+		if (!res || !(await bcrypt.compare(data.password, res.password))) return fail(400, { error: 'invalid username or password' });
 
-    const token = await userCache('uuid', { username: res.username }, 60 * 60 * 24);
+		const token = await userCache('uuid', { username: res.username }, 60 * 60 * 24);
 
-    if (!token) return fail(500, { error: 'unable to login' });
-    cookies.set('auth_token', token, {
-      path: '/',
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 60 * 60 * 24
-    });
+		if (!token) return fail(500, { error: 'unable to login' });
+		cookies.set('auth_token', token, {
+			path: '/',
+			httpOnly: true,
+			secure: process.env.NODE_ENV === 'production',
+			sameSite: 'strict',
+			maxAge: 60 * 60 * 24
+		});
 
-    throw redirect(303, '/');
-  },
-  signup: async ({ request, cookies }) => {
-    const formData = usersSchema.add.safeParse(Object.fromEntries(await request.formData()));
+		throw redirect(303, '/');
+	},
+	signup: async ({ request, cookies }) => {
+		const formData = usersSchema.add.safeParse(Object.fromEntries(await request.formData()));
 
-    if (!formData.success) return formatZodError(formData.error);
-    const data = formData.data;
+		if (!formData.success) return formatZodError(formData.error);
+		const data = formData.data;
 
-    const hash = await bcrypt.hash(data.password, 10);
-    try {
-      await db.insert(users).values({
-        username: data.username,
-        email: data.email,
-        password: hash
-      });
-    } catch (e) {
-      return formatDbError(e);
-    }
+		const hash = await bcrypt.hash(data.password, 10);
+		try {
+			await db.insert(users).values({
+				username: data.username,
+				email: data.email,
+				password: hash
+			});
+		} catch (e) {
+			return formatDbError(e);
+		}
 
-    const token = await userCache('uuid', { username: data.username }, 60 * 60 * 24);
+		const token = await userCache('uuid', { username: data.username }, 60 * 60 * 24);
 
-    if (!token) return fail(500, { error: 'invalid username or password' });
-    cookies.set('auth_token', token, {
-      path: '/',
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 60 * 60 * 24
-    });
+		if (!token) return fail(500, { error: 'invalid username or password' });
+		cookies.set('auth_token', token, {
+			path: '/',
+			httpOnly: true,
+			secure: process.env.NODE_ENV === 'production',
+			sameSite: 'strict',
+			maxAge: 60 * 60 * 24
+		});
 
-    throw redirect(303, '/');
-  }
+		throw redirect(303, '/');
+	}
 };
