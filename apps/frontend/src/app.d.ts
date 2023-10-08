cd7aee2ca021ca209012232cfb7ec2bfdfc9c60a
mp281x <paldugnachmatteo.dev@gmail.com>
2023-10-08 12:17:19 +0200

    fix

--- apps/frontend/src/app.d.ts
+++ apps/frontend/src/app.d.ts
@@ -1,13 +1,11 @@
 import type { userCache } from 'cache';
 
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
 declare global {
-	namespace App {
-		interface Locals {
-			user: Exclude<Awaited<ReturnType<typeof userCache>>, string | undefined>;
-		}
-	}
+  namespace App {
+    interface Locals {
+      user: Exclude<Awaited<ReturnType<typeof userCache>>, string | undefined>;
+    }
+  }
 }
 
-export {};
+export { };
